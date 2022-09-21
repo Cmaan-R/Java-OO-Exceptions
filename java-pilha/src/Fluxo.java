f8c17ad1e68dc9dba90a2f6ca5801a32feafd0b5
Windows 10 <Windows 10@192.168.200.171>
2022-09-21 08:41:18 -0300

    Update

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -4,19 +4,23 @@ public class Fluxo {
 	public static void main(String[] args) {
 
 		System.out.println("Ini do main");
-		metodo1();
+		try {
+			metodo1();
+
+		} catch (ArithmeticException ex) {
+//			String msg = ex.getMessage();
+//			System.out.println("ArithmeticException " + msg);
+			
+			ex.printStackTrace();
+		}
 		System.out.println("Fim do main");
 	}
 
 	private static void metodo1() {
 
 		System.out.println("Ini do metodo1");
-		try {
-			metodo2();
-			
-		} catch (ArithmeticException ex){
-			System.out.println("ArithmeticException");
-		}
+		metodo2();
+
 		System.out.println("Fim do metodo1");
 	}
 
@@ -26,12 +30,7 @@ public class Fluxo {
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
 
-			try {
 				int a = i / 0;
-
-			} catch (ArithmeticException ex) {
-				System.out.println("Caiu no try / catch");
-			}
 
 		}
 		System.out.println("Fim do metodo2");
