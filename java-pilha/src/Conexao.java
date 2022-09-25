1d8c4b82ae2d6ae3522a00a740c4b828219cc0f8
Windows 10 <Windows 10@DESKTOP-ANGJ5V6>
2022-09-25 15:07:05 -0300

    Update

---
+++ java-pilha/src/Conexao.java
@@ -0,0 +1,17 @@
+
+public class Conexao {
+
+	public Conexao() {
+		System.out.println("Abrindo Conexao");
+	}
+
+	public void leDados() {
+		System.out.println("Recebendo Dados");
+		 throw new IllegalStateException();
+	}
+
+	public void fecha() {
+		System.out.println("Fechando Conexao");
+	}
+
+}
