c02c8102d99f2eb087024ac144828c9108d99fdc
Windows 10 <Windows 10@DESKTOP-ANGJ5V6>
2022-09-25 15:10:25 -0300

    Update

--- java-pilha/src/TesteConexao.java
+++ java-pilha/src/TesteConexao.java
@@ -1,16 +1,19 @@
 
 public class TesteConexao {
-	
+
 	public static void main(String[] args) {
-		
+
+		Conexao con = null;
+
 		try {
-			Conexao con = new Conexao();
+			con = new Conexao();
 			con.leDados();
 		} catch (IllegalStateException ex) {
 			System.out.println("Deu Erro Na Conexão");
+		} finally {
+			con.fecha();
 		}
-		
-		
+
 	}
 
 }
